6b62321ab14149e473d0aea025ef5a0504165a20
Ryan Christiani <ryan@ryanchristiani.com>
2017-09-25 21:04:30 -0400

    Fixing regex

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Transform = require('stream').Transform;
 
 class BoatTransform extends Transform {
 	_transform(chunk, enc, next) {
-		const string = chunk.toString().replace(/[Bb]oat/ig,"⛵️");
+		const string = chunk.toString().replace(/boat/ig,"⛵️");
 		this.push(string);
 		next();
 	}
